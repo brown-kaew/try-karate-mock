fda00b198d16e3a0f7520e9fe649c8901be97ce6
brown-kaew <kaewtibkham@gmail.com>
2023-11-16 19:56:14 +0700

    git add makefile

--- Makefile
+++ Makefile
@@ -1,2 +1,15 @@
 run:
-	rm -f nohup.out && ./local_startup.sh
+	./local_startup.sh
+
+start: run
+
+stop:
+	kill -9 $$(cat start.pid) 2> /dev/null && echo "stopped"
+
+restart: stop start
+
+lt:
+	./load_test.sh
+
+clean:
+	rm -f nohup.out
--- local_startup.sh
+++ local_startup.sh
@@ -5,3 +5,4 @@ echo "$PWD"
 nohup ~/Library/Java/JavaVirtualMachines/temurin-1.8.0_382/Contents/Home/bin/java -Xms1024m -Xmx1024m -XX:MaxMetaspaceSize=256m -Dio.netty.availableProcessors=200 -jar $PWD/karate/karate-1.1.0.jar -p 8085 -m=$PWD/payment-service-mock.feature >> $PWD/nohup.out 2>&1 &
 
 echo "Running pid $(jps -l | grep try-karate-mock)"
+echo "$(jps -l | grep try-karate-mock | awk '{print $1}')" > start.pid
